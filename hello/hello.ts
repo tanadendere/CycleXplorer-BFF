33077d13de9446285205d648fc5afa30e340fb17
tanadendere <tanadendere@gmail.com>
2024-11-26 13:19:44 +0200

    feat: :rocket: Testing deploying when changes are pushed to main

--- hello/hello.ts
+++ hello/hello.ts
@@ -15,7 +15,7 @@ import { api } from "encore.dev/api";
 export const get = api(
   { expose: true, method: "GET", path: "/hello/:name" },
   async ({ name }: { name: string }): Promise<Response> => {
-    const msg = `Hello there, ${name}!`;
+    const msg = `Hello ${name}!`;
     return { message: msg };
   }
 );
